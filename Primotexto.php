f4b79218071bdf4069c62e07a2c83d144c7e4170
crd2i <contact@crd2i.fr>
2018-03-01 12:12:16 +0100

    OK

--- Primotexto.php
+++ Primotexto.php
@@ -1,15 +1,26 @@
 <?php
 namespace Primotexto;
 
-class Primotexto 
+require_once 'primotexto-api-php/baseManager.class.php';
+
+class Primotexto extends SMS
 {
+	public $apiKey;
+
 	public function __construct() 
-	{
-		echo "test";
+	{		
+
 	}	
 
-	public test()
+	public function setApiKey($apiKey)
 	{
-		
+		$this->apiKey =	$apiKey;
+	}
+
+	public function send()
+	{		
+		authenticationManager::setApiKey($this->apiKey);
+
+		return (array) json_decode(messagesManager::messagesSend($this));
 	}
 }
